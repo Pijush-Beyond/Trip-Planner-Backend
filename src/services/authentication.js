297f95e58f6c97cfc7f408e716deaac3b7baf14c
Pijush <itsonlymyeducation@gmail.com>
2021-04-29 19:10:10 +0530

    backend initialized_2

---
+++ src/services/authentication.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+import { log } from 'express-errorlog';
+
+export const signIn = (res, data) => {
+  res.cookie('user', jwt.sign(
+    { data },
+    fs.readFileSync('secret.key'),
+    {
+      algorithm: 'HS256'
+    },
+  ))
+}
+
+export default (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.user) {
+    token = req.cookies.user;
+  }
+
+  if (token) {
+    try {
+      const data = jwt.verify(token, fs.readFileSync('secret.key'));
+      req.user = data.data;
+      next();
+    } catch (e) {
+      res.redirect('/login');
+    }
+  }
+  else
+    res.redirect('/login');
+}
